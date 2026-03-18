c31adba19108919aa1d049f2511148dea09d9e02
kagamirudo <gp492@drexel.edu>
2026-03-18 17:40:06 -0400

    Update repository

--- Project/make_submission.sh
+++ Project/make_submission.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR"
 
-NAME="AudioRestore_submission"
+NAME="gp492_final"
 DEST="/tmp/$NAME"
 ZIP="$SCRIPT_DIR/$NAME.zip"
 
